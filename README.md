35469eb9e7e4d011438f413c69bc8ba0d3a812c8
ajaykumv <ajaykumar.vasireddy@cegeka.com>
2023-09-12 12:17:19 +0200

    Adding the test description

---
+++ README.md
@@ -0,0 +1,1 @@
+Its a test automation project for the LIS team
--- app/src/test/java/ui/Testcases/LisVTC1.java
+++ app/src/test/java/ui/Testcases/LisVTC1.java
@@ -6,11 +6,19 @@ import ui.Steps.Lab400Steps;
 
 public class LisVTC1 extends BaseTestThreading {
 
+  //Testcase details
+  private static final String TESTCASE_SUMMARY     = "Ordval of the order";
+  private static final String TESTCASE_DESCRIPTION = "For any given order id, lab400 steps are executed to do the ORDVAL";
+
+
+  public LisVTC1() {
+    setTestcaseDetails(TESTCASE_SUMMARY, TESTCASE_DESCRIPTION);
+  }
 
   @Test
   public void Testcase() throws Exception {
     Lab400Steps lab400Steps = new Lab400Steps();
-    String orderID ="123.456"; //FIXME Should come from API/other lab400 steps
+    String orderID = "123.456"; //FIXME Should come from API/other lab400 steps
     lab400Steps.ordVal(orderID);
   }
 }
